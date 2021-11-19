7a19f667c3b0f8cf65447bcceebbcc23c7c72c7f
bugarenko <tbuharenko@kse.org.ua>
2021-11-19 13:08:39 +0200

    function add

--- 0-buggy.py
+++ 0-buggy.py
@@ -1,4 +1,16 @@
-first_num = 2
-second_num = 3
-sum = first_num + second_num
-print(sum)
+def sum_numbers(x, y):
+    total = x + y
+    return total
+
+
+first_num = 234
+second_num = 4324
+
+print(sum_numbers(first_num, second_num))
+
+
+
+
+
+
+
