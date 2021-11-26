8f0d2af838afa120485b8780d891a00a507907b3
bugarenko <tbuharenko@kse.org.ua>
2021-11-26 22:08:40 +0200

    fixed.cpp

---
+++ cmake-build-debug/CMakeFiles/Bugarenko.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Bugarenko"
+  "Bugarenko.pdb"
+  "CMakeFiles/Bugarenko.dir/0-buggy.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Bugarenko.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
